e748f78ae0c5696fb6c7003bdb6acdfd49ea27c8
YukiZensho <yukizenshou@gmail.com>
2021-08-10 01:23:20 +0300

    Slithering

--- js/game.js
+++ js/game.js
@@ -2,20 +2,30 @@
 
 
 var Body="<p id=\"text\">Snek</p>";
-var Length=4,speed=5;
+var Length=30,speed=5,k;
 var dot=[{'xpos':200,'ypos':100,'alpha':0.0}],xpos=0,ypos=0;
 //Add the fruit
-Body+="<img id=\"Fruit\" src=\"png/fruit.png\" width=\"20\" height=\"20\">";
+Body+="<div id=\"Fruit\"></div>";
 
 //Initialize Snek
-Body+="<img id=\"S0\" src=\"png/snek.png\" width=\"30\" height=\"30\">";
+Body+="<div id=\"S0\"></div>";
 for(var i=1;i<Length;i++){
-    Body+="<img id=\"S"+i+"\" src=\"png/snek.png\" width=\"20\" height=\"20\">";
+    Body+="<div id=\"S"+i+"\"></div>";
     dot.push({'xpos':195-i*30,'ypos':100,'alpha':0.0});
 }
 document.getElementById("game").innerHTML=Body;
-for(var i=0;i<Length;i++)
+for(var i=0;i<Length;i++){
     document.getElementById("S"+i).style.position="absolute";
+    document.getElementById("S"+i).style.width="20px";
+    document.getElementById("S"+i).style.height="20px";
+    document.getElementById("S"+i).style.margin="0px";
+    document.getElementById("S"+i).style.padding="0px";
+    document.getElementById("S"+i).style.borderRadius="50%";
+    document.getElementById("S"+i).style.backgroundColor="#2f2";
+}
+
+    document.getElementById("S0").style.width="30px";
+    document.getElementById("S0").style.height="30px";
 SnkHead=document.getElementById("S0");
 
 function Game(){
@@ -29,10 +39,13 @@ function Game(){
     SnkHead.style.top =dot[0].ypos+"px";
     SnkHead.style.left=dot[0].xpos+"px";
     for(var i=1;i<Length;i++){
-        if(dot[i].alpha==0)dot[i].alpha=dot[0].alpha;
-        else dot[i].alpha=dot[i].alpha*7/8;
-        dot[i].xpos+=(dot[i-1].xpos>dot[i].xpos)?Math.cos(dot[i].alpha)*speed:-Math.cos(dot[i].alpha)*speed;
-        dot[i].ypos+=(dot[i-1].ypos>dot[i].ypos)?Math.sin(dot[i].alpha)*speed:-Math.sin(dot[i].alpha)*speed;
+        /*if(dot[i].alpha==0)dot[i].alpha=dot[0].alpha;
+        else dot[i].alpha=(dot[i].alpha+dot[i-1].alpha)/2;*/
+        dot[i].alpha=Math.abs(Math.atan((dot[i-1].ypos-dot[i].ypos)/(dot[i-1].xpos-dot[i].xpos)));
+        k=(dot[i-1].ypos>dot[i].ypos)?Math.sin(dot[i].alpha)*speed:-Math.sin(dot[i].alpha)*speed;
+        dot[i].ypos=dot[i-1].ypos-k;
+        k=(dot[i-1].xpos>dot[i].xpos)?Math.cos(dot[i].alpha)*speed:-Math.cos(dot[i].alpha)*speed;
+        dot[i].xpos=dot[i-1].xpos-k;
         document.getElementById("S"+i).style.top =dot[i].ypos+"px";
         document.getElementById("S"+i).style.left=dot[i].xpos+"px";
     }
